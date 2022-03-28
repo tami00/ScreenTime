5cf8950d585f1ce2597136c3850b07325b00be72
Tami Adeduntan <tami.adeduntan@gmal.com>
2022-03-29 00:04:45 +0100

    added review model

---
+++ backend/app/models/review.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Review = mongoose.model(
+  "Review",
+  new mongoose.Schema({
+    reviewId: String,
+    content: String,
+    author: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role"
+      }
+    ],
+    reponseTo: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User"
+        }
+    ]
+  }), {timestamps: true}
+);
+
+module.exports = Review;
